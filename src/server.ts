29abde1b2ac9208d3535a13171b30cfdaf74be4b
Paweł Stefanowicz <nonamenodejs@gmail.com>
2025-02-19 23:52:56 +0200

    Deploy build

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import app from "./index";
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
