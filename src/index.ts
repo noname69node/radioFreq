a9769bae3c22dc5724282f86547f1bad912a14d4
Paweł Stefanowicz <nonamenodejs@gmail.com>
2025-02-20 00:24:38 +0200

    Deploy build

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Frequency routes initialized");
+});
 app.use("/api/frequencies", frequencyRoutes);
 app.use("/api/auth", authRoutes);
 // app.use("/api/auth", authRoutes);
